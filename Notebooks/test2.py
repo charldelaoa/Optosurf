c097745033dbf17dafc79e1df9ef23eef350730a
charldelaoa <120415887+charldelaoa@users.noreply.github.com>
2023-02-20 12:53:49 +0000

    added tkinter

--- Notebooks/test2.py
+++ Notebooks/test2.py
@@ -4,7 +4,8 @@ from bokeh.models import ColumnDataSource
 import random
 import pandas as pd
 import time
-
+from tkinter import *
+import tkinter as tk
 
 # Create initial data
 x = []
@@ -35,8 +36,43 @@ def update_data():
     print('new_data:', new_data)
     source.stream(new_data, rollover=50)
     df.loc[counter-1] = [x[-1], y[-1]]
-    df.to_csv('data.csv', index=False)
+    df.to_csv(file, index=False)
 # Add the plot to the document and define the update interval
 counter = 0
+
+#SETTING UP FILE NAME WINDOW
+def closewindow():
+    root.destroy()
+ 
+def SaveFileName():
+    FileName = str(FileNameEntry.get())
+    FileNameLabel.destroy()
+    FileNameEntry.destroy()
+    FileNameButton.destroy()
+ 
+root = Tk() #opening main window
+ 
+root.wm_attributes("-topmost", 1)
+root.title("1500-1600 Sweep") # naming window
+root.geometry("700x250") # window size
+ 
+#Setting label,entry path and save button for file name
+FileName = tk.StringVar()
+FileNameEntry = Entry(root, width = 50, border = 5 , textvariable = FileName)
+FileNameLabel = Label(root, text = "File Name: ")
+FileNameButton = Button(root, text = "Save" , command = SaveFileName)
+CloseButton = Button(root, text = "Close" , fg = "red" , command = closewindow)
+ 
+ 
+ 
+FileNameLabel.grid(row=0,column=0)
+FileNameEntry.grid(row=0,column=1)
+FileNameButton.grid(row=0,column=2)
+CloseButton.grid(row=5,column=0)
+ 
+root.mainloop()
+
+file = FileName.get()
+
 curdoc().add_root(plot)
 curdoc().add_periodic_callback(update_data, 100)

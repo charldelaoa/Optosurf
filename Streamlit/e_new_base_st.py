5321f56d5822ba544ebd29084ffd48ac6e054494
charldelaoa <120415887+charldelaoa@users.noreply.github.com>
2023-03-29 17:15:55 +0100

    added ann10 plotly

--- Streamlit/e_new_base_st.py
+++ Streamlit/e_new_base_st.py
@@ -1,13 +1,14 @@
-
 import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
 import plotly.express as px
-
+import plotly.graph_objects as go
 import pandas as pd
+import numpy as np
 
-# Load mtcars dataset
-mtcars = pd.read_csv('https://plotly.github.io/datasets/mtcars.csv')
+# 1. Load data
+raw_data = np.loadtxt("data/e/Ann_wafer10_off_diodes.dat", delimiter=',')
+dfAnn = pd.read_csv("data/e/Ann_wafer10_calculated.csv")
 
 # Define app
 app = dash.Dash(__name__)
@@ -17,11 +18,22 @@ app.layout = html.Div([
     html.Div([
         dcc.Graph(
             id='plot1',
-            figure=px.scatter(mtcars, x='wt', y='mpg')
+            figure=go.Figure(data=go.Scatter(
+                x=dfAnn['x'],
+                y=dfAnn['y'],
+                mode='markers',
+                marker=dict(color=dfAnn['z'], colorscale='turbo'),
+                customdata=dfAnn[['z', 'radii', 'angle', 'n']],
+                hovertemplate='x: %{x:.2f} mm<br>y: %{y:.2f} mm<br>z: %{customdata[0]:.2f}<br>radii: %{customdata[1]:.2f}<br>angle: %{customdata[2]:.2f}<br>n: %{customdata[3]}'
+            )).update_layout(
+                height=600,
+                width=600,
+                xaxis_title='x (mm)',
+                yaxis_title='y (mm)',
+                title = 'Ann10',
+                coloraxis_colorbar=dict(title='z', len=0.75, y=0.5)
+            )
         )
-    ], className='col-4'),
-    html.Div([
-        dcc.Graph(id='plot2')
     ], className='col-12'),
     html.Div([
         html.H4('Selected Data'),
@@ -31,33 +43,17 @@ app.layout = html.Div([
 
 # Define callbacks
 @app.callback(
-    Output('plot2', 'figure'),
-    Input('plot1', 'selectedData')
-)
-def display_brushed_plot(selectedData):
-    if selectedData is not None:
-        indices = [point['pointIndex'] for point in selectedData['points']]
-        selected_mtcars = mtcars.iloc[indices]
-        try:
-            fig = px.scatter(selected_mtcars, x='disp', y='hp')
-        except Exception as e:
-            print(str(e))
-            fig = None
-        return fig
-    else:
-        return {}
-
-@app.callback(
     Output('selected_data', 'children'),
     Input('plot1', 'selectedData')
 )
 def display_selected_data(selectedData):
     if selectedData is not None:
         indices = [point['pointIndex'] for point in selectedData['points']]
-        selected_mtcars = mtcars.iloc[indices]
-        return [html.Tr([html.Th(col) for col in selected_mtcars.columns])] + [html.Tr([html.Td(selected_mtcars.iloc[i][col]) for col in selected_mtcars.columns]) for i in range(len(selected_mtcars))]
+        selected_dfAnn = dfAnn.iloc[indices]
+        return [html.Tr([html.Th(col) for col in selected_dfAnn.columns])] + [html.Tr([html.Td(selected_dfAnn.iloc[i][col]) for col in selected_dfAnn.columns]) for i in range(len(selected_dfAnn))]
     else:
         return []
+
 
 # Run app
 if __name__ == '__main__':

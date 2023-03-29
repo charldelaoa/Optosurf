d4e5f921a786d7bfb513be05ca7409817a14a035
charldelaoa <120415887+charldelaoa@users.noreply.github.com>
2023-03-29 17:41:49 +0100

    added subplot

--- Streamlit/e_new_base_st.py
+++ Streamlit/e_new_base_st.py
@@ -3,6 +3,7 @@ from dash import dcc, html
 from dash.dependencies import Input, Output
 import plotly.express as px
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import pandas as pd
 import numpy as np
 
@@ -38,22 +39,45 @@ app.layout = html.Div([
     html.Div([
         html.H4('Selected Data'),
         html.Table(id='selected_data')
-    ], className='col-12')
+    ], className='col-12'),
+    dcc.Graph(id='subplots')
 ], className='row')
 
 # Define callbacks
 @app.callback(
-    Output('selected_data', 'children'),
+    [Output('selected_data', 'children'), Output('subplots', 'figure')],
     Input('plot1', 'selectedData')
 )
 def display_selected_data(selectedData):
     if selectedData is not None:
         indices = [point['pointIndex'] for point in selectedData['points']]
         selected_dfAnn = dfAnn.iloc[indices]
-        return [html.Tr([html.Th(col) for col in selected_dfAnn.columns])] + [html.Tr([html.Td(selected_dfAnn.iloc[i][col]) for col in selected_dfAnn.columns]) for i in range(len(selected_dfAnn))]
-    else:
-        return []
+        selected_data_table = [html.Tr([html.Th(col) for col in selected_dfAnn.columns])] + [html.Tr([html.Td(selected_dfAnn.iloc[i][col]) for col in selected_dfAnn.columns]) for i in range(len(selected_dfAnn))]
 
+        # Create a grid of subplots for each index n in the selected data
+        fig = make_subplots(rows=4, cols=8, subplot_titles=[f"Index n={n}" for n in selected_dfAnn['n'].unique()])
+
+        # Add scatter plots to each subplot
+        for i, n in enumerate(selected_dfAnn['n'].unique()):
+            n_data = raw_data[n]
+            x = np.arange(1,31)
+            y = n_data
+            fig.add_trace(go.Scatter(
+                x=x,
+                y=y,
+                ), row=1+i//8, col=1+i%8)
+
+        # Update the layout of the subplots
+        fig.update_layout(
+            height=800,
+            width=1200,
+            title=f"Index n={n}",
+        )
+
+        return selected_data_table, fig
+
+    else:
+        return '', {}
 
 # Run app
 if __name__ == '__main__':

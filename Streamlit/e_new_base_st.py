0ce42cc11d5760043e763205c80552a424b27124
charldelaoa <120415887+charldelaoa@users.noreply.github.com>
2023-04-03 12:36:17 +0100

    new base

--- Streamlit/e_new_base_st.py
+++ Streamlit/e_new_base_st.py
@@ -10,11 +10,11 @@ import dash_bootstrap_components as dbc
 
 # 1. Load data
 # dfAnn = pd.read_csv("data/e/Ann_wafer10_calculated.csv")
-raw_data = np.loadtxt("data/e/Ann_wafer7_off_diodes.dat", delimiter=',')
-dfAnn = pd.read_csv("data/e/Ann_wafer7_calculate.csv")
+raw_data_Ann_7 = np.loadtxt("data/e/Ann_wafer7_off_diodes.dat", delimiter=',')
+dfAnn_7 = pd.read_csv("data/e/Ann_wafer7_calculate.csv")
 raw_data_Ann_10 = np.loadtxt("data/e/Ann_wafer10_off_diodes.dat", delimiter=',')
-na = 764
-dfAnnbase = pd.read_csv("data/e/Ann_wafer10_calculated.csv")
+dfAnn_10 = pd.read_csv("data/e/Ann_wafer10_calculated.csv")
+na = 8012
 
 # 2. Define app
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CERULEAN])
@@ -29,11 +29,11 @@ dbc.Row([
         dcc.Graph(
             id='plot1',
             figure = go.Figure(data=go.Scatter(
-                x=dfAnn['x'],
-                y=dfAnn['y'],
+                x=dfAnn_7['x'],
+                y=dfAnn_7['y'],
                 mode='markers',
-                marker=dict(color=dfAnn['z'], colorscale='turbo'),
-                customdata=dfAnn[['z', 'n']],
+                marker=dict(color=dfAnn_7['z'], colorscale='turbo'),
+                customdata=dfAnn_7[['z', 'n']],
                 hovertemplate='x: %{x:.2f} mm<br>y: %{y:.2f} mm<br>z: %{customdata[0]:.2f}<br>n: %{customdata[1]}'
             )).update_layout(
                 height=700,
@@ -72,35 +72,35 @@ def display_selected_data(selectedData):
     if selectedData is not None:
         # Get selected data df
         indices = [point['pointIndex'] for point in selectedData['points']]
-        selected_dfAnn = dfAnn.iloc[indices]
-        selected_dfAnn = selected_dfAnn.sort_values(by=['z'])
-        selected_data_table = [html.Tr([html.Th(col) for col in selected_dfAnn.columns])] + [html.Tr([html.Td(selected_dfAnn.iloc[i][col]) for col in selected_dfAnn.columns]) for i in range(len(selected_dfAnn))]
+        print(indices)
+        selected_df = dfAnn_7.iloc[indices]
+        selected_df = selected_df.sort_values(by=['z'])
+        selected_data_table = [html.Tr([html.Th(col) for col in selected_df.columns])] + [html.Tr([html.Td(selected_df.iloc[i][col]) for col in selected_df.columns]) for i in range(len(selected_df))]
         
         # Get Ann10 data
         base_function = raw_data_Ann_10[na]
-        Aqbase = dfAnnbase.loc[na]['z']
+        Aqbase = dfAnn_10.loc[na]['z']
 
         # Calculate the rows and cols of subplots
-        n_unique = len(selected_dfAnn['n'].unique())
+        n_unique = len(selected_df['n'])
         cols = min(n_unique, 4)  # Maximum of 3 columns
         rows = (n_unique - 1) // cols + 1
-        titles = list(selected_dfAnn['z'])
+        titles = list(selected_df['z'])
         fig = make_subplots(rows=rows, cols=cols, horizontal_spacing=0.05, vertical_spacing=0.05, subplot_titles=titles)
 
         # Add scatter plots to each subplot
-        for i, n in enumerate(selected_dfAnn['n']):
-            n_data = raw_data[n]
+        for i, n in enumerate(selected_df['n']):
             x = np.arange(-15.5,16.5)
-            y = n_data
-            Aq = selected_dfAnn.loc[n]['z']
-            fig.add_trace(go.Scatter(x=x, y=base_function, line = dict(width=4, color = '#7CB518'), name = f'base; n: {n}',), 
+            y = raw_data_Ann_7[n]
+            Aq = selected_df.loc[n]['z']
+            fig.add_trace(go.Scatter(x=x, y=base_function, line = dict(width=4, color = '#7CB518'), name = f'Aqbase {Aqbase:.4f}',), 
                           row=1+i//cols, col=1+i%cols)
-            fig.add_trace(go.Scatter(x=x, y=base_function, mode='markers', name=f'base points ({Aqbase:.3f})',marker=dict(size=14,symbol='triangle-up-dot', color = '#E08DAC')), 
-                          row=1+i//cols, col=1+i%cols)
+            # fig.add_trace(go.Scatter(x=x, y=base_function, mode='markers', name=f'base points ({Aqbase:.3f})',marker=dict(size=14,symbol='triangle-up-dot', color = '#E08DAC')), 
+            #               row=1+i//cols, col=1+i%cols)
             fig.add_trace(go.Scatter(x=x, y=y, line = dict(width=4, color= '#3F7CAC', dash='dash'), name = f'Aq: {Aq:.2f}; n: {n}',), 
                           row=1+i//cols, col=1+i%cols)
-            fig.add_trace(go.Scatter(x=x, y=y, mode='markers', name = f'Aq: {Aq:.2f}', marker = dict(size=11, color='#6A7FDB'),), 
-                          row=1+i//cols, col=1+i%cols)
+            # fig.add_trace(go.Scatter(x=x, y=y, mode='markers', name = f'Aq: {Aq:.2f}', marker = dict(size=11, color='#6A7FDB'),), 
+            #               row=1+i//cols, col=1+i%cols)
             
     
             fig.update_yaxes(title_text=f"Intensity ({n})", row=1+i//cols, col=1+i%cols,)
